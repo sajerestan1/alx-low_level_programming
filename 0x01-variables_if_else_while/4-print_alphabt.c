119e8758bba62a3e3ed03c8b665dd68f33ff1824
sajerestan@gmail.com <sajerestan@gmail.com>
2022-10-16 02:14:38 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char lowAlpha;
+
+
+	for (lowAlpha = 'a'; lowAlpha <= 'z'; lowAlpha++)
+	{
+		if (lowAlpha != 'q' && lowAlpha != 'e')
+		{
+			putchar(lowAlpha);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
