c32125eaa57befcd35ed9f03d14f917db5b393de
sajerestan@gmail.com <sajerestan@gmail.com>
2022-10-15 22:13:38 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,7 @@ int main(void)
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-		printf(n);
+		(n > 0) ? printf("%d is positive") : (n == 0) ?
+		printf("%d is zero") : printf("%d is negative");
 		return (0);
 	}
