7c42396ea7263aabf8602eb843e7f9f9ba72ebd0
sajerestan@gmail.com <sajerestan@gmail.com>
2022-10-19 22:26:46 -0700

    Task 0 & 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "holberton.h"
+#include "main.c"
 /**
- * main - Entry point
- * Description: prints 'Holberton \n'
+ * main : prints _putchar and a new line
  * Return: Always 0 (success)
  */
 int main(void)
 {
-char str[] = "Holberton\n";
-int i = 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-while (str[i] != '\0')
-	{
-	char c = str[i];
-
-	_putchar(c);
-	i++;
-	}
-return (0);
+	return (0);
 }
