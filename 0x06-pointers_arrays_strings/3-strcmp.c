990b6ea13534f4252d324652cc853833df02563e
Stanley Sajere <53940361+sajerestan1@users.noreply.github.com>
2022-10-26 23:33:40 +0000

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,53 +1,25 @@
-#include "main.h"
-#include <stdio.h>
-/**
- * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
-		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
-	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-		*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-		bg--, size_r--;
-	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+	#include "main.h"
+/**
+*_strcmp - compares two strings, this good to add
+*@s1: A pointer to a character that will be changed
+*@s2: A pointer to a character that will also be changed/modified/updated
+*Return: dest
+*/
+int _strcmp(char *s1, char *s2)
+
+{
+
+char *str_one = s1;
+char *str_two = s2;
+
+
+while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+{
+str_one++;
+str_two++;
 }
+
+return (*str_one - *str_two);
+}
+
