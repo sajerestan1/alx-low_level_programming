67c42fcfebe121cc1cbbd16aa7b831cb8b7f05be
Stanley Sajere <sajerestan@gmail.com>
2022-12-11 08:34:27 +0100

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,14 +22,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if(fd == -1)
+	if (fd == -1)
 		return (0);
 	buffer = malloc(letters * sizeof(char));
 	buffer[letters] = '\0';
 	do {
 		tmp = read(fd, buffer, letters);
 		read_size += tmp;
-	} while (tmp); 
+	} while (tmp);
 	close(fd);
 	if (read_size != letters)
 		return (0);
